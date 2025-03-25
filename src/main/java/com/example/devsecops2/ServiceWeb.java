b7edbb86eb8f9923fd8eb95e38a3047294a0b4be
raissa mable <eyram-raissa.mable@efrei.net>
2025-03-25 16:08:22 +0100

    fist commit

--- src/main/java/com/example/devsecops2/ServiceWeb.java
+++ src/main/java/com/example/devsecops2/ServiceWeb.java
@@ -18,6 +18,8 @@ public class ServiceWeb {
         cars.add(car);
         car = new Car("CC33DD", "peugeot", 500);
         cars.add(car);
+        car = new Car("DD44EE", "Lancia", 500);
+        cars.add(car);
     }
 
 
